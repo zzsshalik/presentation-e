516236097e18e4063af7039415f2312bd17ff275
Yauhen Shalik <yauhen_shalik@epam.com>
2024-04-22 10:10:11 +0400

    fix styles

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import 'reveal.js/dist/reveal.css';
+import 'reveal.js/dist/theme/black.css';
 
 let deck = new Reveal({
    plugins: [ Markdown ]
