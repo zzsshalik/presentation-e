fe0743fd8e768c0cfa33868e86aa9d3314a56ca7
Yauhen Shalik <yauhen_shalik@epam.com>
2024-04-22 21:22:56 +0400

    test2

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Reveal from 'reveal.js';
-import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
-import 'reveal.js/dist/reveal.css';
-import 'reveal.js/dist/theme/black.css';
-
-let deck = new Reveal({
-   plugins: [ Markdown ]
-})
-deck.initialize();
